05391330aa372dd713334d2c0f6510e10305908b
Ricardo <rp_mira@hotmail.com>
2023-08-01 14:47:51 -0300

    projeto to_do

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef} from 'react'
 
-function App() {
+export default function App() {
+
+  // STATE
+    const [listaTarefas, setListaTarefas] = useState(()=>{return[]})  // Retorna um Array vazio
+    const [tarefa, setTarefa] = useState(()=>{return ''})             // Retorna uma string vazia
+
+  // REF
+    const idTarefa = useRef(0)  // Id da tarefa
+    const inputRef = useRef()   // Vai dar controle sobre o elemento HTML
+
+  // METODOS
+    function adicionarTarefa(){
+
+      if(inputRef.current.value.length >= 1){                                           // Verifica se campo esta vazio
+        setListaTarefas(old=>{return [...old, {id: idTarefa.current, texto: tarefa}]})  // Adiciona um item na lista e mantem os antigos
+        idTarefa.current++                                                              // Incrementa ID
+        setTarefa('')                                                                   // Apaga tarefa que foi digitado no campo
+        inputRef.current.focus()                                                        // Foca no campo
+      }else{
+        alert("Digite uma tarefa no campo!")                                            // Caixa de Mensagem
+        inputRef.current.focus()                                                        // Foca no campo
+      }
+
+    }
+
+    function limparTarefas(){
+      setListaTarefas(old=>{return []}) // Retorna Array vazio, reseta lista
+      idTarefa.current = 0              // Reseta Id
+    }
+
+    function removerTarfa(id){
+      const tmp = listaTarefas.filter(tarefa => tarefa.id !== id) // Retorna lista com todos os elemento que o ID seja diferente do que foi passado
+      setListaTarefas(tmp)                                        // Atualiza lista
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Gestor de Tarefas</h1>
+      <hr />
+      <input ref={inputRef} type='text' value={tarefa} onChange={(evento)=>{setTarefa(evento.target.value)}} />
+      <div>
+        <button onClick={adicionarTarefa}>Adicionar</button>
+        <button onClick={limparTarefas}>Limpar Tudo</button>
+      </div>
+      <hr />
+      <p>Tarefas: </p>
+      {listaTarefas.map((item)=>{
+        return <p key={item.id}>{item.texto} - <button onClick={()=>{removerTarfa(item.id)}}>Remover</button> </p>
+      })}
+    </>
+  )
 }
-
-export default App;
